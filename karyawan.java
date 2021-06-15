cbfbb6143f5798b1817e0e0ece4d747265f9620c
sujanoko1 <69161616+sujanoko1@users.noreply.github.com>
2021-06-15 10:18:44 +0700

    Create karyawan.java

---
+++ karyawan.java
@@ -0,0 +1,13 @@
+public class Karyawan {
+
+    String NIP;
+    String nama;
+    String jenisKelamin;
+
+    public void masukKerja() {
+        System.out.println("Masuk kerja");
+    }
+
+    public void beriNama(String nama) {
+        this.nama = nama;
+    }
